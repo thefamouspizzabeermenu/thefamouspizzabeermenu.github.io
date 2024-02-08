625a4e03702ce8aeb38c37b0c77806f5c2bff26c
deleitempanadas <deleite.empanadas2012@gmail.com>
2024-02-07 19:35:01 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,141 @@
+window.sr = ScrollReveal();
+
+	sr.reveal('.logo', {
+		duration: 2000, //tiempo
+		delay:200,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+
+
+sr.reveal('.imagen2', {
+		duration: 2000, //tiempo
+		delay:200,
+		origin: 'right', //origen
+		distance: '-100px', //distancia
+
+	});
+
+
+sr.reveal('.imagen3', {
+		duration: 2000, //tiempo
+		delay:200,
+		origin: 'top', //origen
+		distance: '-200px', //distancia
+
+	});
+
+
+sr.reveal('.imagen4', {
+		duration: 2000, //tiempo
+		origin: 'right', //origen
+		distance: '-100px', //distancia
+
+	});
+sr.reveal('.imagen5', {
+		duration: 2000, //tiempo
+		delay:200,
+		origin: 'top', //origen
+		distance: '-200px', //distancia
+
+	});
+sr.reveal('.imagen6', {
+		duration: 2000, //tiempo
+		delay:200,
+		origin: 'top', //origen
+		distance: '-200px', //distancia
+
+	});
+sr.reveal('.imagen7', {
+		duration: 2000, //tiempo
+		delay:300,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+
+sr.reveal('.imagen8', {
+		duration: 2000, //tiempo
+		delay:300,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+sr.reveal('.imagen10', {
+		duration: 2000, //tiempo
+		origin: 'right', //origen
+		distance: '-100px', //distancia
+
+	});
+sr.reveal('.imagen9', {
+		duration: 2000, //tiempo
+		delay:200,
+		origin: 'bottom', //origen
+		distance: '-200px', //distancia
+
+	});
+
+sr.reveal('.imagen11', {
+		duration: 2000, //tiempo
+		delay:200,
+		origin: 'top', //origen
+		distance: '-200px', //distancia
+
+	});
+
+sr.reveal('.imagen12', {
+		duration: 2000, //tiempo
+		delay:200,
+		origin: 'bottom', //origen
+		distance: '-200px', //distancia
+
+	});
+sr.reveal('.imagen13', {
+		duration: 2000, //tiempo
+		delay:200,
+		origin: 'top', //origen
+		distance: '-200px', //distancia
+
+	});
+
+	sr.reveal('.menu', {
+		duration: 1500, //tiempo
+		origin: 'bottom', //origen
+		distance: '-50px' //distancia
+
+
+	}); 
+
+
+	sr.reveal('.imagen4', {
+		duration: 1500, //tiempo
+		origin: 'right', //origen
+		distance: '-100px', //distancia
+
+	});
+
+
+
+	sr.reveal('.piepagina', {
+		duration: 1500, //tiempo
+		origin: 'bottom', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	}); 
+
+
+	sr.reveal('.table-responsive', {
+		duration: 1500, //tiempo
+		origin: 'bottom', //origen
+		distance: '-100px', //distancia
+		
+
+	});
